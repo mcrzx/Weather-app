e741a32a275316db167334e0b0e30d13d91f5c42
Maria <maria.lorincz@tracknamic.com>
2023-04-04 09:26:34 +0300

    search optimised

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ function Input() {
     <div className='flex flex-row justify-center my-6'>
         <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
             <input 
-            placeholder='Search for a city...'
+            placeholder='Search...'
             type="text"
              className='text-xl font-light p-2 w-full shadow-xl focus:outline-none  capitalize placeholder:lowercase' />
       
