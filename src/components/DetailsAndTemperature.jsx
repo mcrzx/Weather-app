b541e04e5ee2fe67407fc26bb210d35bd10bf49b
Maria <maria.lorincz@tracknamic.com>
2023-04-04 13:10:25 +0300

    obtaining rise, set, high, low data from API

--- src/components/DetailsAndTemperature.jsx
+++ src/components/DetailsAndTemperature.jsx
@@ -8,7 +8,7 @@ import{
     UilSun,
     UilSunset,
 } from "@iconscout/react-unicons";
-import { iconUrlFromCode } from '../services/weatherService';
+import { formatToLocalTime, iconUrlFromCode } from '../services/weatherService';
 
 function DetailsAndTemperature({weather: {
     details, icon, temp_min, temp_max, sunrise, sunset, speed, humidity, feels_like, timezone, temp
@@ -47,16 +47,16 @@ function DetailsAndTemperature({weather: {
         </div>
         <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'> 
         <UilSun/>
-        <p className='font-light'>Rise: <span className='font-medium ml-1'>06:45 AM</span></p>
+        <p className='font-light'>Rise: <span className='font-medium ml-1'>{formatToLocalTime(sunrise, timezone, "hh:mm a")}</span></p>
         <p className='font-light'>|</p>
         <UilSunset/>
-        <p className='font-light'>Set: <span className='font-medium ml-1'>07:35 PM</span></p>
+        <p className='font-light'>Set: <span className='font-medium ml-1'>{formatToLocalTime(sunset, timezone, "hh:mm a")}</span></p>
         <p className='font-light'>|</p>
         <UilSun/>
-        <p className='font-light'>High: <span className='font-medium ml-1'>45°</span></p>
+        <p className='font-light'>High: <span className='font-medium ml-1'>{`${temp_max.toFixed()}°`}</span></p>
         <p className='font-light'>|</p>
         <UilSun/>
-        <p className='font-light'>Low: <span className='font-medium ml-1'>40°</span></p>
+        <p className='font-light'>Low: <span className='font-medium ml-1'>{`${temp_min.toFixed()}°`}</span></p>
         <p className='font-light'>|</p>
         </div>
     </div>
