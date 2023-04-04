abe1a304a83830a448e3549046c0e56e22af5dfd
Maria <maria.lorincz@tracknamic.com>
2023-04-04 12:48:49 +0300

    pulling the date, timezone from API

--- src/components/LocationAndTime.jsx
+++ src/components/LocationAndTime.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { formatToLocalTime } from '../services/weatherService'
 
 function LocationAndTime({weather: {dt, timezone, name, country}}) {
   return (
 <div>
     <div className=' flex items-center justify-center my-6'>
         <p className='text-white text-xl font-extralight'>
-            Tuesday, 31 May 2022 | Local time: 12:46 PM
+           {formatToLocalTime(dt, timezone)}
         </p>
     </div>
     <div className=' flex items-center justify-center my-3'>
