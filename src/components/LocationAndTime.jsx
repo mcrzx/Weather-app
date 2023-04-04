2079f5e3e08ad0e68943d11ea96cf6e63a30f6bb
Maria <maria.lorincz@tracknamic.com>
2023-04-04 12:46:12 +0300

    call from API

--- src/components/LocationAndTime.jsx
+++ src/components/LocationAndTime.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function LocationAndTime() {
+function LocationAndTime({weather: {dt, timezone, name, country}}) {
   return (
 <div>
     <div className=' flex items-center justify-center my-6'>
@@ -9,7 +9,7 @@ function LocationAndTime() {
         </p>
     </div>
     <div className=' flex items-center justify-center my-3'>
-    <p className='text-white text-3xl font-medium'> Berlin, DE</p>
+    <p className='text-white text-3xl font-medium'> {`${name},${country}`}</p>
 </div> 
 </div>
  )
