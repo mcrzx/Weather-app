16a97415f9d4b07f429c46c1ba5405a7860a28a1
Maria <maria.lorincz@tracknamic.com>
2023-04-04 12:45:57 +0300

    setting up useState

--- src/App.js
+++ src/App.js
@@ -6,21 +6,38 @@ import LocationAndTime from './components/LocationAndTime';
 import DetailsAndTemperature from './components/DetailsAndTemperature';
 import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [query, setQuery] = useState({q: 'berlin'})
+const [units, setUnits] = useState('metric')
+const [weather, setWeather] = useState(null)
+
+useEffect (() => {
   const fetchWeather = async () => {
-     const data = await getFormattedWeatherData( {q:'london'})
-     console.log(data);
-  }
+    await getFormattedWeatherData({...query, units}).then((data) =>
+      {
+        setWeather(data);
+      });
+  };
 fetchWeather();
+}, [query, units])
+
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
      <TopPageButtons/>
      <Input/>
-     <LocationAndTime/>
-     <DetailsAndTemperature/>
+
+     {weather && (
+      <div> 
+        <LocationAndTime weather= {weather}/>
+     <DetailsAndTemperature weather = {weather}/>
      <Forecast title='hourly forecast'/>
      <Forecast title='daily forecast'/>
+      </div>
+     )}
+     
     </div>
   );
 }
