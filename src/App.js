1e10437902f17bac164e80be1b041007b4b272ab
Maria <maria.lorincz@tracknamic.com>
2023-04-04 09:20:26 +0300

    Imported new pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopPageButtons from './components/TopPageButtons';
+import Input from './components/Input';
+import LocationAndTime from './components/LocationAndTime';
+import DetailsAndTemperature from './components/DetailsAndTemperature';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
      <TopPageButtons/>
-     <h1>Weather app</h1>
+     <Input/>
+     <LocationAndTime/>
+     <DetailsAndTemperature/>
+     <Forecast title='hourly forecast'/>
+     <Forecast title='daily forecast'/>
     </div>
   );
 }
