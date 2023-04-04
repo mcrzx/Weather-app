ee4e0d9d46bc1049b99eb4112fa8255f6f973935
Maria <maria.lorincz@tracknamic.com>
2023-04-04 15:59:52 +0300

    implementing seach method

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
-function Input() {
+import React, { useState } from 'react'
+import { UilSearch, UilLocationPoint, UilHeart } from '@iconscout/react-unicons'
+function Input({setQuery, units, setUnits}) {
+  const [city, setCity] = useState('');
+
+  const handleSearchClick = () => {
+    if (city !== '') setQuery({q: city}) 
+  }
   return (
     <div className='flex flex-row justify-center my-6'>
         <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
             <input 
-            placeholder='Search...'
+            value = {city}
+            onChange= {(e) => setCity(e.currentTarget.value)}
+            placeholder='Enter location'
             type="text"
              className='text-xl font-light p-2 w-full shadow-xl focus:outline-none  capitalize placeholder:lowercase' />
       
-        <UilSearch size={25} className='text-white cursor-pointer  transition ease-out hover:scale-125 '/>
+        <UilSearch size={25} className='text-white cursor-pointer  transition ease-out hover:scale-125 ' onClick={handleSearchClick}/>
         <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+        <UilHeart size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
         </div>
 
         <div className='flex flex-row w-1/4 items-center justify-center'>
-            <button name='metric' className='text-xl text-white font-light'>°C </button>
+            <button name='metric' className='text-xl text-white font-light transition ease-out hover:scale-125'>°C </button>
             <p className='text-xl text-white mx-1'>|</p>
-            <button name='imperial' className='text-xl text-white font-light'>°F </button>
+            <button name='imperial' className='text-xl text-white font-light transition ease-out hover:scale-125'>°F </button>
         </div>
      </div>
 )}
