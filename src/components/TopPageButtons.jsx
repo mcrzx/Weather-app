3735808c862a4382c4619148f11538f8b12ddbd1
Maria <maria.lorincz@tracknamic.com>
2023-04-04 09:24:36 +0300

    Top page quick links

--- src/components/TopPageButtons.jsx
+++ src/components/TopPageButtons.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
 function TopPageButtons() {
-  return (
-    <div>TopPageButtons</div>
-  )
+    const cities = [
+    {
+      id:1,
+      title: 'London'
+    },
+    {
+      id:2,
+      title: 'Bucharest'
+    },
+    {
+      id:3,
+      title: 'Tokyo'
+    },
+    {
+      id:4,
+      title: 'Toronto'
+    },
+    {
+      id:5,
+      title: 'Paris'
+    }
+  
+  
+  
+  
+  
+  
+  ]
+
+
+   return  <div className='flex item-center justify-around my-6'>
+    {cities.map((city) => (
+        <button key={city.id} className='text-white text-lg font-medium'>
+          {city.title}
+          </button>
+    ))}
+   </div>
+  
 }
 
 export default TopPageButtons
