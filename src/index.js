f843485d604e18a358d5daa0e11b39964e9dc41b
Maria <maria.lorincz@tracknamic.com>
2023-04-06 20:20:33 +0300

    created routing for the navbar elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
- 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-  
-);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Favourites from './components/Favourites';
 
+ReactDOM.render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/components/Favourites" element={<Favourites />} />
+        </Routes>
+    </Router>,
+    document.getElementById('root')
+);
