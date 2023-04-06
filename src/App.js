9b979d6f1a1b83b7334005368df0d3f910a79e67
Maria <maria.lorincz@tracknamic.com>
2023-04-06 20:20:09 +0300

    beautified the code

--- src/App.js
+++ src/App.js
@@ -1,80 +1,70 @@
-import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopPageButtons from './components/TopPageButtons';
-import Input from './components/Input';
-import LocationAndTime from './components/LocationAndTime';
-import DetailsAndTemperature from './components/DetailsAndTemperature';
-import Forecast from './components/Forecast';
-import getFormattedWeatherData from './services/weatherService';
-import { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar';
-import Preloader from './components/Preloader';
-import Main from './components/Main';
-
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopPageButtons from "./components/TopPageButtons";
+import Input from "./components/Input";
+import LocationAndTime from "./components/LocationAndTime";
+import DetailsAndTemperature from "./components/DetailsAndTemperature";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import Preloader from "./components/Preloader";
+import Main from "./components/Main";
 
 function App() {
-const [query, setQuery] = useState({q:'Oradea'})
-const [units, setUnits] = useState('metric')
-const [weather, setWeather] = useState(null)
+  const [query, setQuery] = useState({ q: "Oradea" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
-useEffect (() => {
-  const fetchWeather = async () => {
-    const message = query.q ? query.q: 'current location.';
-    toast.info('Fetching weather for ' + message);
-    await getFormattedWeatherData({...query, units}).then((data) =>
-      {
-        toast.success(`Successfully fetched weather for ${data.name}, ${data.country}`)
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+      toast.info("Fetching weather for " + message);
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}`
+        );
         setWeather(data);
       });
-  };
-fetchWeather();
-}, [query, units])
+    };
+    fetchWeather();
+  }, [query, units]);
 
-const formatBackground = () => {
-  if (!weather) return "from-cyan-700 to-blue-700"; //default background till the data is fetched
-  const threshold = units === "metric" ? 20 : 60;
-  if (weather.temp <= threshold) 
-  return "from-cyan-700 to-blue-700";
-  return "from-yellow-700 to-orange-700";
-};
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700"; //default background till the data is fetched
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+    return "from-yellow-700 to-orange-700";
+  };
 
   return (
     <div className="overlay">
+      <div className="main">
+        <Main />
+        <div
+          className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+        >
+          {/* <TopPageButtons setQuery={setQuery}/> */}
+          <Preloader />
+          <Navbar />
+          <Input setQuery={setQuery} units={units} setUnits={setUnits} />
+          {weather && (
+            <div>
+              <LocationAndTime weather={weather} />
+              <DetailsAndTemperature weather={weather} />
 
-    <div className="main">
-    <Main />
-    <div className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
-     {/* <TopPageButtons setQuery={setQuery}/> */}
+              <Forecast title="hourly forecast" items={weather.hourly} />
+              <Forecast title="daily forecast" items={weather.daily} />
+            </div>
+          )}
 
- 
-     <>    <Preloader/></>
-  
-     <Navbar />
-
-     <Input setQuery={setQuery} units={units} setUnits={setUnits}/>
-    
-     {weather && (
-      <div> 
-        <LocationAndTime weather= {weather}/>
-     <DetailsAndTemperature weather = {weather}/>
-     
-     <Forecast title='hourly forecast' items={weather.hourly}/>
- 
-     <Forecast title='daily forecast' items={weather.daily}/>
-    
-      </div>
-     )}
-
-<ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
-
-    </div>
+          <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+        </div>
       </div>
     </div>
-       
   );
- 
 }
 
 export default App;
