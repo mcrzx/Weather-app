a53b48644bd9c70bda714db55b5b0e4069a8f8a7
Maria <maria.lorincz@tracknamic.com>
2023-04-11 12:56:16 +0300

    added, search, suggestion, search handle updte, API/ endpoint

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,61 +1,126 @@
-import React, { useState } from 'react'
-import { UilSearch, UilLocationPoint, UilHeart } from '@iconscout/react-unicons'
+import React, { useState } from 'react';
+import { UilSearch, UilLocationPoint, UilHeart } from '@iconscout/react-unicons';
 import { toast } from 'react-toastify';
+import Autosuggest from 'react-autosuggest';
 
-function Input({setQuery, units, setUnits}) {
-  const [city, setCity] = useState('');
+function Input({ setQuery, units, setUnits }) {
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   const handleUnitsChange = (e) => {
-    const selectedUnit = e.currentTarget.name
+    const selectedUnit = e.currentTarget.name;
     if (units !== selectedUnit) setUnits(selectedUnit);
-  } 
+  };
+
   const handleSearchClick = () => {
-    if (city !== '') {
-      setQuery({q: city})
-      toast.success('Successfully fetched weather')
-    }
-  }
-
-  const handleFaveClick = () => {
-    toast.info('Added to favourites')
-  }
-  const handleLocationClick = () => {
-    toast.info ('Fetching users location.')
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        toast.success('Location fetched!')
-        let lat = position.coords.latitude
-        let lon = position.coords.longitude
-
-        setQuery({
-          lat,lon,
-        })
-      })
-    }
-  }
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSearchClick();
+    if (value !== '') {
+      setQuery({ q: value });
+      toast.success('Successfully fetched weather');
     }
   };
 
+  const handleFaveClick = () => {
+    toast.info('Added to favourites');
+  };
+
+  const handleLocationClick = () => {
+    toast.info('Fetching user location.');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        toast.success('Location fetched!');
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        setQuery({ lat, lon });
+      });
+    }
+  };
+
+  const handleSuggestionsFetchRequested = ({ value }) => {
+    fetch(`https://api.openweathermap.org/data/2.5/find?q=${value}&type=like&appid=91ded8148b316886689e7522bee4fc11`)
+      .then((response) => response.json())
+      .then((data) => {
+        const suggestions = data.list.map((result) => {
+          const cityName = result.name;
+          const countryName = result.sys.country;
+          return countryName ? `${cityName}, ${countryName}` : cityName;
+        });
+        setSuggestions(suggestions);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
+  
+
+  const handleSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const handleChange = (_, { newValue }) => {
+    setValue(newValue);
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setQuery({ q: suggestion });
+    setValue(suggestion);
+    setSuggestions([]);
+  };
+
+  const getSuggestionValue = (suggestion) => suggestion;
+
+  const renderSuggestion = (suggestion) => (
+    <button className="p-2 w-full text-left" onClick={() => handleSuggestionClick(suggestion)}>
+      {suggestion}
+    </button>
+  );
+
+  const inputProps = {
+    placeholder: 'Enter location',
+    value,
+    onChange: handleChange,
+    style: {
+      fontSize: '24px',
+      height: '48px',
+      borderRadius: '8px',
+      paddingLeft: '16px',
+    },
+    onKeyDown: (e) => {
+      if (e.key === 'Enter') {
+        handleSearchClick();
+      }
+    },
+  };
+  
+
   return (
-    <div className='container mx-auto sm:px-6 md:px-8 lg:px-10'>
-      <div className='flex flex-col sm:flex-row justify-center my-6'>
-        <div className='flex flex-col sm:flex-row w-full sm:w-3/4 items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4'>
-          <input 
-            value={city}
-            onChange={(e) => setCity(e.currentTarget.value)}
-            placeholder='Enter location'
-            type='text'
-            className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'
-            onKeyPress={handleKeyPress}
+    <div className="container mx-auto sm:px-6 md:px-8 lg:px-10">
+      <div className="flex flex-col sm:flex-row justify-center my-6">
+        <div className="flex flex-col sm:flex-row w-full sm:w-3/4 items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={handleSuggestionsFetchRequested}
+            onSuggestionsClearRequested={handleSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
           />
-          <div className='flex flex-row items-center justify-center space-x-4'>
-            <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-125 ' onClick={handleSearchClick}/>
-            <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' onClick={handleLocationClick}/>
-            <UilHeart size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' onClick={handleFaveClick}/>
+          <div className="flex flex-row items-center justify-center space-x-4">
+            <UilSearch
+              size={25}
+              className="text-white cursor-pointer transition ease-out hover:scale-125 "
+              onClick={handleSearchClick}
+            />
+            <UilLocationPoint
+              size={25}
+              className="text-white cursor-pointer transition ease-out hover:scale-125"
+              onClick={handleLocationClick}
+            />
+            <UilHeart
+              size={25}
+              className="text-white cursor-pointer transition ease-out hover:scale-125"
+              onClick={handleFaveClick}
+            />
           </div>
         </div>
 
