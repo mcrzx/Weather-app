f2ff2396ca9a2612bfed8cc1a7ecd47ef86b33dc
Maria <maria.lorincz@tracknamic.com>
2023-04-12 08:15:42 +0300

    updated favourites list: select, delete, responsive

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -2,44 +2,90 @@ import React from "react";
 import { formatToLocalTime } from "../services/weatherService";
 import Main from "./Main";
 import Navbar from "./Navbar";
-import { toast, ToastContainer } from 'react-toastify';
-
+import { toast, ToastContainer } from "react-toastify";
+import { useFavouriteCities } from "../utils/ZustandStore";
 import { UilTrashAlt } from "@iconscout/react-unicons";
+import { useEffect } from "react";
+import { useRemoveFavouriteCity } from "../utils/ZustandStore";
+import { useState } from "react";
 
 function Favourites({ weather }) {
   const { dt, timezone } = weather || {};
+  const favouriteCities = useFavouriteCities();
+  const removeFavouriteCity = useRemoveFavouriteCity();
 
-  
+  const [selectedCities, setSelectedCities] = useState([]);
+
   const handleDeleteClick = () => {
-    toast.info('City has been removed succesfully from the list!');
-  }
-  
+    if (selectedCities.length === 0) {
+      return;
+    }
+    const cityNames = selectedCities.join(", ");
+    toast.info(`${cityNames} have been removed successfully from the list!`);
+    selectedCities.forEach(removeFavouriteCity);
+    setSelectedCities([]);
+  };
+
+  const handleCheckboxChange = (event, city) => {
+    if (event.target.checked) {
+      setSelectedCities([...selectedCities, city]);
+      localStorage.setItem("selectedCity", city);
+    } else {
+      setSelectedCities(selectedCities.filter((c) => c !== city));
+      localStorage.removeItem("selectedCity");
+    }
+  };
+
   return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-4 md:px-16 lg:px-24 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <Main />
       <Navbar />
-      <ToastContainer/>
-      <div>
-        <div className="flex items-center justify-center my-6">
-          <p className="text-white text-xl font-extralight">
-            {dt && timezone ? formatToLocalTime(dt, timezone) : ""}
-          </p>
-        </div>
+      <ToastContainer />
+      <div className="my-6 flex justify-center">
+        <p className="text-white text-xl font-extralight">
+          {dt && timezone ? formatToLocalTime(dt, timezone) : ""}
+        </p>
       </div>
-      <div className="text-white text-3xl font-medium flex flex-row justify-center my-20">
+      <div className="text-white text-3xl font-medium text-center my-8">
         Favourite Cities
       </div>
       <div>
         <hr className="my-2" />
-        <div className="flex items-center justify-start my-6">
-          <p className="text-white font-medium uppercase">Saved cities</p>
-          <UilTrashAlt  size={25} className='text-white cursor-pointer 
-           transition ease-out hover:scale-125 items-end' onClick={handleDeleteClick} />
-
+        <div className="flex justify-center my-6">
+          {/* <hr className="my-2" /> */}
         </div>
-        <hr className="my-2" />
+        <div className="flex flex-col justify-center">
+          {favouriteCities?.map((city, index) => (
+            <div
+            className="flex flex-col md:flex-row items-center justify-between my-6"
+            key={index}
+            >
+      <div className="flex items-center justify-start mb-4 md:mb-0">
+                <input
+                  type="checkbox"
+                  checked={selectedCities.includes(city)}
+                  onChange={(event) => handleCheckboxChange(event, city)}
+                />
+        <p className="text-white text-xl md:text-2xl font-medium ml-3">{city}</p>
+              </div>
+            </div>
+          ))}
+          {selectedCities.length > 0 && (
+            <div className="flex justify-center my-4">
+              <button
+                onClick={handleDeleteClick}
+                className="text-white font-medium px-4 py-2 rounded-lg bg-red-500 hover:bg-red-600 transition ease-out"
+                >
+                <UilTrashAlt className="inline-block mr-1" size="20" />
+                Delete 
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
+
 }
+
 export default Favourites;
