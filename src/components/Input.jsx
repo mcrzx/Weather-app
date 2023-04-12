d729a52a3751e4e949443fe1044efda46412b25d
Maria <maria.lorincz@tracknamic.com>
2023-04-12 08:16:09 +0300

    updated search

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { UilSearch, UilLocationPoint, UilHeart } from '@iconscout/react-unicons';
-import { toast } from 'react-toastify';
-import Autosuggest from 'react-autosuggest';
+import React, { useState } from "react";
+import {
+  UilSearch,
+  UilLocationPoint,
+  UilHeart,
+} from "@iconscout/react-unicons";
+import { toast } from "react-toastify";
+import Autosuggest from "react-autosuggest";
+import { useCityStore, useFavouriteCityStore } from "../utils/ZustandStore";
 
 function Input({ setQuery, units, setUnits }) {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
-    const [favourites, setFavourites] = useState([]);
-
+  const [favourites, setFavourites] = useState([]);
 
   const handleUnitsChange = (e) => {
     const selectedUnit = e.currentTarget.name;
@@ -15,23 +19,25 @@ function Input({ setQuery, units, setUnits }) {
   };
 
   const handleFaveClick = () => {
-    toast.info('Added to favourites');
+    toast.info("Added to favourites");
     setFavourites((prevFavourites) => [...prevFavourites, value]);
+    useFavouriteCityStore.getState().addFavouriteCity(value);
+    console.log(useFavouriteCityStore.getState().favouriteCities);
   };
 
   const handleSearchClick = () => {
-    if (value !== '') {
+    if (value !== "") {
       setQuery({ q: value });
-      toast.success('Successfully fetched weather');
+      toast.success("Successfully fetched weather");
+      useCityStore.getState().addCity(value);
     }
   };
 
-
   const handleLocationClick = () => {
-    toast.info('Fetching user location.');
+    toast.info("Fetching user location.");
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        toast.success('Location fetched!');
+        toast.success("Location fetched!");
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
         setQuery({ lat, lon });
@@ -40,15 +46,17 @@ function Input({ setQuery, units, setUnits }) {
   };
 
   const handleSuggestionsFetchRequested = ({ value }) => {
-    fetch(`https://api.openweathermap.org/data/2.5/find?q=${value}&type=like&sort=population&cnt=10&appid=91ded8148b316886689e7522bee4fc11`)
+    fetch(
+      `https://api.openweathermap.org/data/2.5/find?q=${value}&type=like&sort=population&cnt=10&appid=91ded8148b316886689e7522bee4fc11`
+    )
       .then((response) => response.json())
       .then((data) => {
-        const suggestions = data.list.map((result) => `${result.name}, ${result.sys.country}`);
-        setSuggestions(suggestions);
+        const suggestions = data.list.map(
+          (result) => `${result.name}, ${result.sys.country}`
+        );
+        setSuggestions([...new Set(suggestions)]);
       });
   };
-  
-  
 
   const handleSuggestionsClearRequested = () => {
     setSuggestions([]);
@@ -67,28 +75,30 @@ function Input({ setQuery, units, setUnits }) {
   const getSuggestionValue = (suggestion) => suggestion;
 
   const renderSuggestion = (suggestion) => (
-    <button className="p-2 w-full text-left" onClick={() => handleSuggestionClick(suggestion)}>
+    <button
+      className="p-2 w-full text-left text-white text-xl"
+      onClick={() => handleSuggestionClick(suggestion)}
+    >
       {suggestion}
     </button>
   );
 
   const inputProps = {
-    placeholder: 'Enter location',
+    placeholder: "Enter location",
     value,
     onChange: handleChange,
     style: {
-      fontSize: '24px',
-      height: '48px',
-      borderRadius: '8px',
-      paddingLeft: '16px',
+      fontSize: "24px",
+      height: "48px",
+      borderRadius: "8px",
+      paddingLeft: "16px",
     },
     onKeyDown: (e) => {
-      if (e.key === 'Enter') {
+      if (e.key === "Enter") {
         handleSearchClick();
       }
     },
   };
-  
 
   return (
     <div className="container mx-auto sm:px-6 md:px-8 lg:px-10">
@@ -121,15 +131,25 @@ function Input({ setQuery, units, setUnits }) {
           </div>
         </div>
 
-        <div className='flex flex-row w-1/4 items-center justify-center md:justify-end'>
-            <button name='metric' className='text-xl text-white font-light transition ease-out hover:scale-125 md:inline-block hidden'
-            onClick={handleUnitsChange}>°C </button>
-            <p className='text-xl text-white mx-1 hidden md:inline-block'>|</p>
-            <button name='imperial' className='text-xl text-white font-light transition ease-out hover:scale-125 md:inline-block hidden'
-            onClick={handleUnitsChange}>°F </button>
+        <div className="flex flex-row w-1/4 items-center justify-center md:justify-end">
+          <button
+            name="metric"
+            className="text-xl text-white font-light transition ease-out hover:scale-125 md:inline-block hidden"
+            onClick={handleUnitsChange}
+          >
+            °C{" "}
+          </button>
+          <p className="text-xl text-white mx-1 hidden md:inline-block">|</p>
+          <button
+            name="imperial"
+            className="text-xl text-white font-light transition ease-out hover:scale-125 md:inline-block hidden"
+            onClick={handleUnitsChange}
+          >
+            °F{" "}
+          </button>
         </div>
-     </div>
-     </div>
+      </div>
+    </div>
   );
 }
 
